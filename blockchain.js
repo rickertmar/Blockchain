103c2383b45a4682b434c08b0b140e823b881ffc
Marian Rickert <marian.rickert@gmail.com>
2022-11-07 16:59:31 +0100

    chain validation

--- blockchain.js
+++ blockchain.js
@@ -1,14 +1,14 @@
 class Block {
-    constructor(index, timestemp, data, previousHash = '') {
+    constructor(index, timestemp, transaction, preHash = '') {
         this.index = index;
         this.timestemp = timestemp;
-        this.data = data;
-        this.previousHash = previousHash;
+        this.transaction = transaction;
+        this.preHash = preHash;
         this.hash = '';
     }
 
-    calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestemp + JSON.stringify(this.data)).toString();
+    calcHash() {
+        return SHA256(this.index + this.preHash + this.timestemp + JSON.stringify(this.transaction)).toString();
     }
 
 }
@@ -19,7 +19,7 @@ class Blockchain {
     }
 
     createGenisisBlock() {
-        return new Block(0, "07.11.2022", "Genisis block", "0");
+        return new Block(0, "07.11.2022", "Genisis-Block", "0");
     }
 
     getLatestBlock() {
@@ -27,8 +27,25 @@ class Blockchain {
     }
 
     addBlock(newBlock) {
-        newBlock.previousHash = newBlock.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.preHash = newBlock.getLatestBlock().hash;
+        newBlock.hash = newBlock.calcHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const preBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calcHash()) {
+                return false;
+            }
+
+            if (currentBlock.preHash !== preBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
